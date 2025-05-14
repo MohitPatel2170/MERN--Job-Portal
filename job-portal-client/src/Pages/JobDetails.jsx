f2420e9e63d8466013f8eb89d34ec2430216150c
Mohit Patel <mohit.patel2170@gmail.com>
2025-05-14 21:28:41 +0530

    Updated Browers List

--- job-portal-client/src/Pages/JobDetails.jsx
+++ job-portal-client/src/Pages/JobDetails.jsx
@@ -1,39 +1,40 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import Swal from 'sweetalert2';
-import PageHeader from '../components/PageHeader';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Swal from "sweetalert2";
+import PageHeader from "../components/PageHeader";
 
 const JobDetails = () => {
-    const {id} = useParams();
-    const [job, setJob] = useState([])
-    useEffect(() => {
+  const { id } = useParams();
+  const [job, setJob] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:5001/all-jobs/${id}`)
+      .then((res) => res.json())
+      .then((data) => setJob(data));
+  }, []);
 
-    fetch(`http://localhost:5001/all-jobs/${id}`).then(res => res.json()).then (data => setJob(data))
-    }, [])
-
-    const handleApply = async() => {
-        const { value: url } = await Swal.fire({
-            input: "url",
-            inputLabel: "URL address",
-            inputPlaceholder: "Enter the URL"
-          });
-          if (url) {
-            Swal.fire(`Entered URL: ${url}`);
-          }
-
+  const handleApply = async () => {
+    const { value: url } = await Swal.fire({
+      input: "url",
+      inputLabel: "URL address",
+      inputPlaceholder: "Enter the URL",
+    });
+    if (url) {
+      Swal.fire(`Entered URL: ${url}`);
     }
+  };
 
   return (
-    <div className='max-w-screen-2xl container justify-center'>
-        <PageHeader title={"Single Job Page"} path={"Single Job"}/>
-        <h2>JobDetails: {id}
+    <div className="max-w-screen-2xl container justify-center">
+      <PageHeader title={"Single Job Page"} path={"Single Job"} />
+      <h2>
+        JobDetails: {id}
         <h1>{job.jobTitle}</h1>
-
-        <button className='bg-blue-500 px-8 text-white ' onClick={handleApply}>Apply Now</button>
-
-        </h2>
+        <button className="bg-blue-500 px-8 text-white " onClick={handleApply}>
+          Apply Now
+        </button>
+      </h2>
     </div>
-  )
-}
+  );
+};
 
 export default JobDetails;
